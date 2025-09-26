6d667f632d80c915c756b0935d380823ff36bc29
its-mrarsikk <mr.arsikoff@gmail.com>
2025-09-26 18:45:40 +0700

    feat(server): add graceful http shutdown, minimal error handling

--- server/httpserver.go
+++ server/httpserver.go
@@ -9,14 +9,30 @@ import (
 	"github.com/its-mrarsikk/fedup/shared"
 )
 
+type HttpServerChannels struct {
+	Err chan error
+}
+
 func handlePing(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "fedupd %s (go %s)\n", shared.Version, runtime.Version())
 }
 
-func RunServer(port int) {
+// graceful shutdown logic from https://stackoverflow.com/a/42533360
+// i found it by accident while looking for docs on ListenAndServe, but good to have anyway lol
+func RunServer(port int, ch *HttpServerChannels) *http.Server {
+	srv := &http.Server{Addr: fmt.Sprintf(":%d", port)}
+
 	http.HandleFunc("/ping", handlePing)
+
+	go func() {
+		// always returns error. ErrServerClosed on graceful close
+		if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+			log.Printf("ListenAndServe(): %T: %v", err, err)
+			ch.Err <- err
+		}
+	}()
 
 	log.Printf("Listening on 0.0.0.0:%d", port)
 
-	log.Print(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+	return srv
 }
--- server/main.go
+++ server/main.go
@@ -1,15 +1,66 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
+	"net/http"
+	"os"
+	"os/signal"
 	"runtime"
+	"strings"
+	"syscall"
+	"time"
 
 	"github.com/its-mrarsikk/fedup/shared"
 )
 
+// graceful shutdown logic from https://stackoverflow.com/a/42533360
+// i found it by accident while looking for docs on ListenAndServe, but good to have anyway lol
+func stopHttp(srv *http.Server) {
+	log.Printf("Shutting down HTTP server")
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil && err != http.ErrServerClosed {
+		panic(err) // failure/timeout shutting down the server gracefully
+	}
+}
+
 func main() {
 	log.Printf("fedupd %s (source code under LGPL v2.1) with Go %s", shared.Version, runtime.Version())
-	go RunServer(4545)
-	for {
-	} // keep it busy until termination
+
+	httpChannels := HttpServerChannels{Err: make(chan error)}
+	mainShouldQuit := make(chan error)
+
+	port := 4545
+
+	go func() {
+		err := <-httpChannels.Err
+		switch {
+		case strings.Contains(err.Error(), "address already in use"):
+			log.Printf("Port %d is already in use (is fedupd already running?)", port)
+			mainShouldQuit <- fmt.Errorf("Port in use")
+		default:
+			log.Printf("The HTTP server encountered an error: %s", err)
+			mainShouldQuit <- fmt.Errorf("HTTP error: %w", err)
+		}
+	}()
+
+	signalCh := make(chan os.Signal, 2)
+	signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM)
+	go func() {
+		mainShouldQuit <- fmt.Errorf("Got signal %s", <-signalCh)
+	}()
+
+	srv := RunServer(port, &httpChannels)
+
+	quitReason := <-mainShouldQuit
+
+	if quitReason != nil {
+		log.Printf("Exit requested with reason: %s", quitReason)
+	}
+
+	stopHttp(srv)
+
+	log.Printf("All done, clocking out.")
 }
