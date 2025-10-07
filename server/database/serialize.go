e234a827efd27562f17cad9a58dd18aa51dbe818
its-mrarsikk <mr.arsikoff@gmail.com>
2025-10-07 16:13:41 +0700

    feat(server/database): add Feed deserialization, nil safety in FeedSerialize
    git add -p strikes again

--- server/database/serialize.go
+++ server/database/serialize.go
@@ -1,13 +1,83 @@
 package database
 
-import "github.com/its-mrarsikk/fedup/shared/rss"
+import (
+	"database/sql"
+	"fmt"
+	"net/url"
+
+	"github.com/its-mrarsikk/fedup/shared/rss"
+)
+
+func safeURLParse(s sql.NullString) *url.URL {
+	if !s.Valid {
+		return nil
+	}
+	u, err := url.Parse(s.String)
+	if err != nil {
+		return nil
+	}
+	return u
+}
 
 func FeedSerialize(f *rss.Feed) ([]any, string) {
+	var link, fetchFrom string
+	if f.Link != nil {
+		link = f.Link.String()
+	} else {
+		link = ""
+	}
+	if f.FetchFrom != nil {
+		fetchFrom = f.FetchFrom.String()
+	} else {
+		fetchFrom = ""
+	}
+
 	return []any{f.DatabaseID,
 		f.Title,
 		f.Description,
-		f.Link.String(),
-		f.FetchFrom.String(),
+		link,
+		fetchFrom,
 		f.Language,
 		f.TTL}, "(?,?,?,?,?,?,?)"
+}
+
+func FeedDeserialize(r *sql.Row) (*rss.Feed, error) {
+	var dbid int
+	var title, description string
+	var link, fetchFrom, language sql.NullString
+	var ttl sql.NullInt64
+
+	var urlLink, urlFetchFrom *url.URL
+	var strLanguage string
+	var intTTL int
+
+	err := r.Scan(&dbid, &title, &description, &link, &fetchFrom, &language, &ttl)
+	if err != nil {
+		return nil, fmt.Errorf("failed to scan row: %w", err)
+	}
+
+	urlLink = safeURLParse(link)
+	urlFetchFrom = safeURLParse(fetchFrom)
+
+	if language.Valid {
+		strLanguage = language.String
+	} else {
+		strLanguage = ""
+	}
+
+	if ttl.Valid {
+		intTTL = int(ttl.Int64)
+	} else {
+		intTTL = 0
+	}
+
+	return &rss.Feed{
+		DatabaseID:  dbid,
+		Title:       title,
+		Description: description,
+		Link:        urlLink,
+		FetchFrom:   urlFetchFrom,
+		Language:    strLanguage,
+		TTL:         intTTL,
+	}, nil
 }
