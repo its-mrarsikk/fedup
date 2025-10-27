c7900fc6e87734e1eb3325cb69de605d828e2300
its-mrarsikk <mr.arsikoff@gmail.com>
2025-10-27 20:02:05 +0700

    feat(database): add caching to feeds table, starred to items table, add enclosures table, default_read view

--- server/database/sql/init.sql
+++ server/database/sql/init.sql
@@ -1,16 +1,15 @@
--- Table: feeds
 CREATE TABLE IF NOT EXISTS feeds (
     id INTEGER PRIMARY KEY,
     title TEXT NOT NULL,
     description TEXT NOT NULL,
-	link TEXT,
-	fetchFrom TEXT,
+	link TEXT, -- HTML human-readable feed
+	fetchFrom TEXT UNIQUE, -- RSS feed
     language TEXT,
-    ttl INTEGER
+    ttl INTEGER NOT NULL DEFAULT 60,
+    etag TEXT,
+    lastModified TEXT -- RFC 3339
 );
 
-
--- Table: items
 CREATE TABLE IF NOT EXISTS items (
     id INTEGER PRIMARY KEY,
     feed_id INTEGER NOT NULL,
@@ -19,10 +18,20 @@ CREATE TABLE IF NOT EXISTS items (
     description TEXT,
     link TEXT,
     author TEXT,
-    pubDate TEXT,
+    pubDate TEXT, -- RFC 3339
     read BOOLEAN NOT NULL DEFAULT 0,
-    enclosure_url TEXT,
-    enclosure_type TEXT,
-    enclosure_length INTEGER,
-    FOREIGN KEY(feed_id) REFERENCES feeds(id)
+    starred BOOLEAN NOT NULL DEFAULT 0,
+    FOREIGN KEY(feed_id) REFERENCES feeds(id) ON DELETE CASCADE
 );
+
+CREATE TABLE IF NOT EXISTS enclosures (
+    id INTEGER PRIMARY KEY,
+    item_id INTEGER NOT NULL,
+    type TEXT NOT NULL, -- MIME type of the enclosure
+    url TEXT NOT NULL UNIQUE,
+    -- length is not relevant in the modern age. it's not the size that matters, right?
+    filePath TEXT NOT NULL,
+    FOREIGN KEY(item_id) REFERENCES items(id) ON DELETE CASCADE
+);
+
+CREATE VIEW default_read AS SELECT * FROM items WHERE read = 0 OR starred = 1;
