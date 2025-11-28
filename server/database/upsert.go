9cbd6e1bdbb69541c05935a79f49c3b809f90b07
its-mrarsikk <mr.arsikoff@gmail.com>
2025-11-28 17:58:30 +0700

    feat(database): use raw sql for upsert

--- server/database/upsert.go
+++ server/database/upsert.go
@@ -3,66 +3,63 @@ package database
 import (
 	"context"
 	"fmt"
+	"time"
 
 	"github.com/its-mrarsikk/fedup/shared/rss"
 	"gorm.io/gorm"
 )
 
-// Constants for Upsert conditions.
-const ()
+// UpsertFeed inserts or updates a Feed. If `recurse` is true, the items and
+// their enclosures are also upserted
+func (db *Database) UpsertFeed(feed rss.Feed, recurse bool, ctx context.Context) error {
+	const stmt = `
+	INSERT INTO feeds(created_at, updated_at, deleted_at, title, description, link, fetch_from, language, ttl, e_tag, last_modified)
+	VALUES (?, ?, NULL, ?, ?, ?, ?, ?, ?, ?, ?)
+	ON CONFLICT(fetch_from) DO UPDATE
+	SET created_at=excluded.created_at,
+	updated_at=excluded.updated_at,
+	title=excluded.title,
+	description=excluded.description,
+	link=excluded.link,
+	fetch_from=excluded.fetch_from,
+	language=excluded.language,
+	ttl=excluded.ttl,
+	e_tag=excluded.e_tag,
+	last_modified=excluded.last_modified;
+`
 
-// Upsert updates or inserts `m` in the database. `where` is a WHERE clause condition
-// that is used to check for existence.
-// Example:
-//
-//	upsert(db, feed, "id = ?, fetch_from = ?", feed.ID, "https://example.com")
-func upsert[T any](db *gorm.DB, m T, where string, args ...any) error {
-	if where == "" {
-		return fmt.Errorf("expected non-empty where")
-	}
+	err := gorm.G[rss.Feed](db.DB).Exec(ctx, stmt,
+		time.Now(), time.Now(), // created_at, updated_at
+		feed.Title, feed.Description, feed.Link,
+		feed.FetchFrom, feed.Language,
+		feed.TTL, feed.ETag, feed.LastModified)
 
-	ctx := context.Background()
-
-	w := gorm.G[T](db).Where(where, args...)
-	rows, err := w.Updates(ctx, m)
 	if err != nil {
-		return fmt.Errorf("failed to update object: %w", err)
+		return fmt.Errorf("failed to execute sql: %w", err)
 	}
 
-	if rows == 0 {
-		err := gorm.G[T](db).Create(ctx, &m)
-		if err != nil {
-			return fmt.Errorf("failed to create object: %w", err)
-		}
-		return nil
-	}
+	if recurse {
 
-	// gorm doesn't update zero values, so we need a separate case for read and starred going from true to false
-	// and yea this isn't very generic but whatever
-	// if i, ok := any(m).(rss.Item); ok {
-	// 	_, err := w.Update(ctx, "read", i.Read)
-	// 	if err != nil {
-	// 		return fmt.Errorf("failed to update object (read): %w", err)
-	// 	}
-	// 	_, err = w.Update(ctx, "starred", i.Starred)
-	// 	if err != nil {
-	// 		return fmt.Errorf("failed to update object (starred): %w", err)
-	// 	}
-	// }
+	}
 
 	return nil
 }
 
-// UpsertFeed inserts or updates a Feed. If `recurse` is true, the items and
-// their enclosures are also upserted
-func (db *Database) UpsertFeed(feed rss.Feed, recurse bool, where string, args ...any) error {
-	if err := upsert(db.DB, feed, where, args...); err != nil {
-		return fmt.Errorf("failed to upsert feed: %w", err)
-	}
-
-	if recurse {
-		// TODO
-	}
-
-	return nil
+func (db *Database) UpsertItem(item rss.Item, recurse bool) {
+	_ = `
+	INSERT INTO items(id, created_at, updated_at, deleted_at, guid, title, description, link, author, pub_date, read, starred)
+	VALUES(?, ?, ?, NULL, ?, ?, ?, ?, ?, ?, ?, ?)
+	ON CONFLICT(id, guid) DO UPDATE
+	SET id=excluded.id,
+	created_at=excluded.created_at,
+	updated_at=excluded.updated_at,
+	guid=excluded.guid,
+	title=excluded.title,
+	description=excluded.description,
+	link=excluded.link,
+	author=excluded.author,
+	pub_date=excluded.pub_date,
+	read=excluded.read,
+	starred=excluded.starred;
+`
 }
