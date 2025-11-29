5c65cea771189ce6e056e90c451fda83591fb152
its-mrarsikk <mr.arsikoff@gmail.com>
2025-11-29 12:59:26 +0700

    feat(database): make upsert set the id

--- server/database/upsert.go
+++ server/database/upsert.go
@@ -11,7 +11,7 @@ import (
 
 // UpsertFeed inserts or updates a Feed. If `recurse` is true, the items and
 // their enclosures are also upserted
-func (db *Database) UpsertFeed(feed rss.Feed, recurse bool, ctx context.Context) error {
+func (db *Database) UpsertFeed(feed *rss.Feed, recurse bool, ctx context.Context) error {
 	const stmt = `
 	INSERT INTO feeds(created_at, updated_at, deleted_at, title, description, link, fetch_from, language, ttl, e_tag, last_modified)
 	VALUES (?, ?, NULL, ?, ?, ?, ?, ?, ?, ?, ?)
@@ -25,34 +25,44 @@ func (db *Database) UpsertFeed(feed rss.Feed, recurse bool, ctx context.Context)
 	language=excluded.language,
 	ttl=excluded.ttl,
 	e_tag=excluded.e_tag,
-	last_modified=excluded.last_modified;
+	last_modified=excluded.last_modified
+	RETURNING id;
 `
 
-	err := gorm.G[rss.Feed](db.DB).Exec(ctx, stmt,
+	id, err := gorm.G[uint](db.DB).Raw(stmt,
 		time.Now(), time.Now(), // created_at, updated_at
 		feed.Title, feed.Description, feed.Link,
 		feed.FetchFrom, feed.Language,
-		feed.TTL, feed.ETag, feed.LastModified)
+		feed.TTL, feed.ETag, feed.LastModified).Take(ctx)
 
 	if err != nil {
 		return fmt.Errorf("failed to execute sql: %w", err)
 	}
 
-	if recurse {
+	feed.ID = id
 
+	if recurse {
+		for _, item := range feed.Items {
+			item.Feed = *feed
+			item.FeedID = id
+			err := db.UpsertItem(&item, true, ctx)
+			if err != nil {
+				return fmt.Errorf("failed to upsert item: %w", err)
+			}
+		}
 	}
 
 	return nil
 }
 
-func (db *Database) UpsertItem(item rss.Item, recurse bool) {
-	_ = `
-	INSERT INTO items(id, created_at, updated_at, deleted_at, guid, title, description, link, author, pub_date, read, starred)
-	VALUES(?, ?, ?, NULL, ?, ?, ?, ?, ?, ?, ?, ?)
-	ON CONFLICT(id, guid) DO UPDATE
-	SET id=excluded.id,
-	created_at=excluded.created_at,
+func (db *Database) UpsertItem(item *rss.Item, recurse bool, ctx context.Context) error {
+	const stmt = `
+	INSERT INTO items(created_at, updated_at, deleted_at, feed_id, guid, title, description, link, author, pub_date, read, starred)
+	VALUES(?, ?, NULL, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+	ON CONFLICT(guid) DO UPDATE
+	SET created_at=excluded.created_at,
 	updated_at=excluded.updated_at,
+	feed_id=excluded.feed_id
 	guid=excluded.guid,
 	title=excluded.title,
 	description=excluded.description,
@@ -60,6 +70,22 @@ func (db *Database) UpsertItem(item rss.Item, recurse bool) {
 	author=excluded.author,
 	pub_date=excluded.pub_date,
 	read=excluded.read,
-	starred=excluded.starred;
+	starred=excluded.starred
+	RETURNING id;
 `
+
+	id, err := gorm.G[uint](db.DB).Raw(stmt,
+		time.Now(), time.Now(),
+		item.FeedID, item.GUID,
+		item.Title, item.Description,
+		item.Link, item.Author,
+		item.PubDate, item.Read, item.Starred).Take(ctx)
+
+	if err != nil {
+		return fmt.Errorf("failed to execute sql: %w", err)
+	}
+
+	item.ID = id
+
+	return nil
 }
